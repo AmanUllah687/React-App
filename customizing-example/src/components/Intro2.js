c80e97cb4fa921296c08aa194c2277523b666029
AmanUllah687 <Amanch687@gmail.com>
2024-03-08 14:16:22 +0500

    Initial commit

---
+++ customizing-example/src/components/Intro2.js
@@ -0,0 +1,12 @@
+function Intro2() {
+    return(
+        <div className="blog-post-intro">
+            <h2>Why I love frontend web development</h2>
+            <div>
+            <p>In this blog post, I'll list 10 reasons why i love to work as a front end web developer.</p>
+            <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+export default Intro2;
---
+++ customizing-example/src/components/Promo.js
@@ -0,0 +1,13 @@
+function Promo() {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss the deal!</h1>
+            </div>
+            <div>
+                <h2>Subcribe to my newsletter and get all the the shop items at 50% off!</h2>
+            </div>
+        </div>
+    );
+};
+export default Promo;
