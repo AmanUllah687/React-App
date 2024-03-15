eb0c7d4f29fdb8329869d82dec3e37feb6593524
AmanUllah687 <Amanch687@gmail.com>
2024-03-15 11:15:23 +0500

    Added heading in README.md file

---
+++ Desktop/customizing-example/customizing-example/src/components/Intro1.js
@@ -0,0 +1,12 @@
+function Intro1() {
+    return (
+        <div className="blog-post-intro">
+        <h2>I've become a React developer!</h2>
+        <div>
+            <p>I've completed the React basic course and I'am happy to annouce that I'am now a junior React developer!</p>
+            <p className="link">Read more...</p>
+        </div>
+        </div>
+    );
+};
+export default Intro1;
---
+++ Desktop/customizing-example/customizing-example/src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+        <nav className="Nav">
+            <ul>
+            <li>Home</li>
+            <li>Articles</li>
+            <li>About</li>
+            <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+export default Nav;
